071a8967e073e6243fa63304e08dbc59c131e041
Joe Rehfuss <joe.rehfuss@gmail.com>
2021-05-21 14:10:25 -0700

    updates

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors")
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const db = require("./models");
 
 const app = express();
@@ -11,68 +12,70 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+app.use(cors());
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/donuts", {
-     useNewUrlParser: true ,
-     useUnifiedTopology: true
-    });
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 
-app.post('/api/donuts',(req,res)=>{
-    db.Donut.create(req.body).then(newDonut=>{
+app.post('/api/donuts', (req, res) => {
+    db.Donut.create(req.body).then(newDonut => {
         res.json(newDonut)
-    }).catch(err=>{
+    }).catch(err => {
         res.status(500).json(err);
     })
 })
 
-app.get("/api/donuts",(req,res)=>{
-    db.Donut.find().then(allDonuts=>{
+app.get("/api/donuts", (req, res) => {
+    db.Donut.find().then(allDonuts => {
         res.json(allDonuts)
-    }).catch(err=>{
+    }).catch(err => {
         res.status(500).json(err);
     })
 })
-app.post('/api/orders',(req,res)=>{
-    db.Order.create(req.body).then(newOrder=>{
+app.post('/api/orders', (req, res) => {
+    db.Order.create(req.body).then(newOrder => {
         res.json(newOrder)
-    }).catch(err=>{
+    }).catch(err => {
         res.status(500).json(err);
     })
 })
 
-app.get("/api/orders",(req,res)=>{
-    db.Order.find().then(allOrders=>{
+app.get("/api/orders", (req, res) => {
+    db.Order.find().then(allOrders => {
         res.json(allOrders)
-    }).catch(err=>{
+    }).catch(err => {
         res.status(500).json(err);
     })
 })
 
-app.put("/api/orders/adddonut/:orderid/:donutid",(req,res)=>{
+app.put("/api/orders/adddonut/:orderid/:donutid", (req, res) => {
     console.log(req.params)
-    db.Order.findOneAndUpdate({_id:req.params.orderid},{$push:{donuts:req.params.donutid}},{new:true}).then(updatedOrder=>{
+    db.Order.findOneAndUpdate({ _id: req.params.orderid }, { $push: { donuts: req.params.donutid } }, { new: true }).then(updatedOrder => {
         res.json(updatedOrder)
-    }).catch(err=>{
+    }).catch(err => {
         res.status(500).json(err);
     })
 })
 
-app.get("/checkout/:orderid",(req,res)=>{
+app.get("/checkout/:orderid", (req, res) => {
     db.Order.findOne({
-        _id:req.params.orderid
-    }).populate("donuts").then(finalOrder=>{
-        const total = finalOrder.donuts.reduce((runningTotal,currentDonut)=>{
-            return runningTotal + currentDonut.price 
-        },0)
-      myObj = finalOrder.toObject()
+        _id: req.params.orderid
+    }).populate("donuts").then(finalOrder => {
+        const total = finalOrder.donuts.reduce((runningTotal, currentDonut) => {
+            return runningTotal + currentDonut.price
+        }, 0)
+        myObj = finalOrder.toObject()
         myObj.totalPrice = total
-        console.log(myObj,total)
+        console.log(myObj, total)
         res.json(myObj)
-    }).catch(err=>{
+    }).catch(err => {
         res.status(500).json(err);
     })
 })
 
-app.listen(PORT,()=>{
-    console.log("listenin on port "+ PORT)
+app.listen(PORT, () => {
+    console.log("listenin on port " + PORT)
 })
